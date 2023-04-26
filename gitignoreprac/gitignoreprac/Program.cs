feb068c060c009b7d4c31dfc7527ab75994ab6c9
daniellarsen77 <daniellarsen127@gmail.com>
2023-04-26 10:13:39 -0400

    Pushed lab

---
+++ gitignoreprac/gitignoreprac/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace gitignoreprac
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
